897fdc932a30e507fd2bae543a8c51a94cb81a8c
minsu <jminsu084@gmail.com>
2022-01-09 16:11:36 +0900

    for문 활용한 01_07 예제 풀이

---
+++ java_algorithm/src/doit_algorithm/ch01/SumForEx_01_07.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doit_algorithm.ch01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Minsu <wjdalstn1450@naver.com>
+ */
+public class SumForEx_01_07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.println("n의 값 : ");
+        int n = sc.nextInt();
+        
+        int sum = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            System.out.print(i + " + ");
+            sum += i;
+            if(i == n)
+                System.out.println(i + " = " + sum);
+        }
+        
+        System.out.println("1부터 " + n + "까지의 합은 " + sum + "입니다.");
+    }
+    
+}
